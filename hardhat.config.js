67fdd6c01722d6ee02f1d49cca62cb39ba722cd9
Drygin <DryginAlexander@gmail.com>
2021-11-28 13:41:19 +0300

    add BSC networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,6 +29,14 @@ module.exports = {
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    bsc_testnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.MORALIS_API_KEY}/bsc/testnet`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    bsc_mainnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${process.env.MORALIS_API_KEY}/bsc/mainnet`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
   },
   contractSizer: {
